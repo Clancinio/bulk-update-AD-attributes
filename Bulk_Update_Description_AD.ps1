bb2d45b4dd015f9ebee9b2995c42383a227717eb
DeanC <dean.clancy91@gmail.com>
2020-06-24 14:03:09 -0700

    Initial Commit

---
+++ Bulk_Update_Description_AD.ps1
@@ -0,0 +1,51 @@
+﻿# This script updates bulk updates the description field in AD from a CSV file
+
+# Propt for Admin credentials and store in $credential variable 
+$credential = Get-Credential
+
+# Load data from file.csv
+$ADUsers = Import-csv C:\Users\myfile.csv # Change this to your CSV location
+
+
+# Count variable for number of users update
+$count = 0
+
+# Server variable - Change this IP to your Domain Controller IP - To get server IP run the command Get-ADDomainController in PowerShell
+$server = "27.0.0.1"
+
+# Go through each row that has user data in the CSV we just imported 
+ForEach($User in $ADUsers)
+{
+    # Ppopulate hash table for Get-ADUser splatting:
+    $GetParams =
+    @{
+        Identity     = $User.Username  #Identity will be the IDIR
+        Server       = $server
+        Credential   = $Credential
+    }
+
+    # Initialize hash table for Set-ADUser splatting:
+    $SetParams =
+    @{
+        Server       = $server
+        Identity     = $User.Username
+        Description  = $User.Description
+        
+    }
+
+    # Check to see if the user already exists in AD. If they do, we update.
+    if ( Get-ADUser @GetParams)
+    {
+         # Set User attributes
+         Set-ADUser @SetParams #-WhatIf
+
+         # Print that the user was updated 
+         Write-Host -ForegroundColor Yellow "$User - User attributes have been updated." 
+
+         # Update Count
+         $count += 1    
+     }
+}
+
+# Print the number of updated users
+Write-Host $count "Users have been updated" -ForegroundColor Green
